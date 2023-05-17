ad59586444039bc537deb3e14012234c7b8224dc
avelous <avelousujiri@gmail.com>
2023-05-17 04:19:04 +0100

    git ignore

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -27,7 +27,7 @@ const config: HardhatUserConfig = {
       },
     ],
   },
-  defaultNetwork: "localhost",
+  defaultNetwork: "mainnet",
   namedAccounts: {
     deployer: {
       // By default, it will take the first Hardhat account as the deployer
