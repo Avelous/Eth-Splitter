8047eb248b863f8c98fbf0c7555f57d32027c076
avelous <avelousujiri@gmail.com>
2023-05-17 04:13:33 +0100

    git ignore

--- packages/nextjs/generated/deployedContracts.ts
+++ packages/nextjs/generated/deployedContracts.ts
@@ -804,7 +804,7 @@ const contracts = {
       chainId: "31337",
       contracts: {
         ETHSplitter: {
-          address: "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0",
+          address: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
           abi: [
             {
               inputs: [],
@@ -1044,7 +1044,7 @@ const contracts = {
           ],
         },
         ERC20Mock: {
-          address: "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9",
+          address: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
           abi: [
             {
               inputs: [
