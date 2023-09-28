f23ba1af11f44f667b57a3f31c441b68cef6e2d3
Shiv Bhonde <shivbhonde04@gmail.com>
2023-09-28 12:20:06 +0530

    migreate to wagmiV1 and viem and daisy v3

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -1,13 +1,10 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import React, { useCallback, useRef, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { Chain, useNetwork, useSwitchNetwork } from "wagmi";
-import * as chains from "wagmi/chains";
-import { Bars3Icon, BugAntIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon, BugAntIcon, MagnifyingGlassIcon, SparklesIcon } from "@heroicons/react/24/outline";
 import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 import { useOutsideClick } from "~~/hooks/scaffold-eth";
-import scaffoldConfig from "~~/scaffold.config";
 
 const NavLink = ({ href, children }: { href: string; children: React.ReactNode }) => {
   const router = useRouter();
@@ -19,7 +16,7 @@ const NavLink = ({ href, children }: { href: string; children: React.ReactNode }
       passHref
       className={`${
         isActive ? "bg-secondary shadow-md" : ""
-      } hover:bg-secondary hover:shadow-md focus:bg-secondary py-1.5 px-3 text-sm rounded-full gap-2`}
+      } hover:bg-secondary hover:shadow-md focus:!bg-secondary active:!text-neutral py-1.5 px-3 text-sm rounded-full gap-2 grid grid-flow-col`}
     >
       {children}
     </Link>
@@ -37,31 +34,6 @@ export const Header = () => {
     useCallback(() => setIsDrawerOpen(false), []),
   );
 
-  const { chains: switchChains, switchNetwork } = useSwitchNetwork();
-  const { chain } = useNetwork();
-
-  const [chainData, setChainData] = useState<Chain[]>();
-
-  const [selectedNetwork, setSelectedNetwork] = useState<string>("");
-
-  function changeTargetNetwork(newNetwork: chains.Chain): void {
-    scaffoldConfig.targetNetwork = newNetwork;
-  }
-
-  console.log(switchChains);
-
-  useEffect(() => {
-    if (switchChains.length > 0) {
-      setChainData(switchChains.filter(item => [1, 11155111, 137, 80001, 10].includes(item.id)));
-    }
-  }, [switchChains]);
-
-  useEffect(() => {
-    if (chain) {
-      setSelectedNetwork(chain.name);
-    }
-  }, [chain]);
-
   const navLinks = (
     <>
       <li>
@@ -73,11 +45,23 @@ export const Header = () => {
           Debug Contracts
         </NavLink>
       </li>
+      <li>
+        <NavLink href="/example-ui">
+          <SparklesIcon className="h-4 w-4" />
+          Example UI
+        </NavLink>
+      </li>
+      <li>
+        <NavLink href="/blockexplorer">
+          <MagnifyingGlassIcon className="h-4 w-4" />
+          Block Explorer
+        </NavLink>
+      </li>
     </>
   );
 
   return (
-    <div className="sticky lg:static top-0 navbar bg-base-100 min-h-0 flex-shrink-0 justify-between z-20 shadow-md shadow-secondary">
+    <div className="sticky lg:static top-0 navbar bg-base-100 min-h-0 flex-shrink-0 justify-between z-20 shadow-md shadow-secondary px-0 sm:px-2">
       <div className="navbar-start w-auto lg:w-1/2">
         <div className="lg:hidden dropdown" ref={burgerMenuRef}>
           <label
@@ -101,45 +85,18 @@ export const Header = () => {
             </ul>
           )}
         </div>
-        <Link href="/" passHref className="hidden lg:flex items-center gap-2 ml-4 mr-6">
+        <Link href="/" passHref className="hidden lg:flex items-center gap-2 ml-4 mr-6 shrink-0">
           <div className="flex relative w-10 h-10">
             <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">ETH & Token Splitter</span>
+            <span className="font-bold leading-tight">Scaffold-ETH</span>
             <span className="text-xs">Ethereum dev stack</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">{navLinks}</ul>
       </div>
       <div className="navbar-end flex-grow mr-4">
-        <select
-          className="select select-sm sm:w-fit w-20 mr-2"
-          style={{ borderWidth: 1, borderColor: chain && (chain as any).color }}
-          onChange={event => {
-            const [name, id] = event.target.value.split("|");
-            switchNetwork?.(+id);
-            console.log(name);
-            name === "Ethereum"
-              ? changeTargetNetwork(chains["mainnet"])
-              : name === "Polygon Mumbai"
-              ? changeTargetNetwork(chains["polygonMumbai"])
-              : changeTargetNetwork(chains[name.toLowerCase()]);
-          }}
-        >
-          <option disabled>Select network</option>
-          {chainData &&
-            chainData.map(data => (
-              <option
-                key={data.name}
-                value={`${data.name}|${data.id}`}
-                style={{ color: (data as any).color }}
-                selected={selectedNetwork === data.name}
-              >
-                {data.name}
-              </option>
-            ))}
-        </select>
         <RainbowKitCustomConnectButton />
         <FaucetButton />
       </div>
--- packages/nextjs/services/web3/wagmiConnectors.tsx
+++ packages/nextjs/services/web3/wagmiConnectors.tsx
@@ -7,7 +7,6 @@ import {
   rainbowWallet,
   walletConnectWallet,
 } from "@rainbow-me/rainbowkit/wallets";
-import { SafeConnector } from "@wagmi/connectors/safe";
 import { configureChains } from "wagmi";
 import * as chains from "wagmi/chains";
 import { alchemyProvider } from "wagmi/providers/alchemy";
@@ -17,17 +16,10 @@ import { burnerWalletConfig } from "~~/services/web3/wagmi-burner/burnerWalletCo
 import { getTargetNetwork } from "~~/utils/scaffold-eth";
 
 const configuredNetwork = getTargetNetwork();
-const burnerConfig = scaffoldConfig.burnerWallet;
+const { onlyLocalBurnerWallet } = scaffoldConfig;
 
 // We always want to have mainnet enabled (ENS resolution, ETH price, etc). But only once.
-const enabledChains = [
-  configuredNetwork,
-  chains.sepolia,
-  chains.mainnet,
-  chains.polygon,
-  chains.polygonMumbai,
-  chains.optimism,
-];
+const enabledChains = configuredNetwork.id === 1 ? [configuredNetwork] : [configuredNetwork, chains.mainnet];
 
 /**
  * Chains for the app
@@ -37,11 +29,11 @@ export const appChains = configureChains(
   [
     alchemyProvider({
       apiKey: scaffoldConfig.alchemyApiKey,
-      priority: 0,
     }),
-    publicProvider({ priority: 1 }),
+    publicProvider(),
   ],
   {
+    // We might not need this checkout https://github.com/scaffold-eth/scaffold-eth-2/pull/45#discussion_r1024496359, will test and remove this before merging
     stallTimeout: 3_000,
     // Sets pollingInterval if using chain's other than local hardhat chain
     ...(configuredNetwork.id !== chains.hardhat.id
@@ -52,31 +44,25 @@ export const appChains = configureChains(
   },
 );
 
+const walletsOptions = { chains: appChains.chains, projectId: scaffoldConfig.walletConnectProjectId };
 const wallets = [
-  metaMaskWallet({ chains: appChains.chains, shimDisconnect: true }),
-  walletConnectWallet({ chains: appChains.chains }),
-  ledgerWallet({ chains: appChains.chains }),
-  braveWallet({ chains: appChains.chains }),
-  coinbaseWallet({ appName: "scaffold-eth-2", chains: appChains.chains }),
-  rainbowWallet({ chains: appChains.chains }),
+  metaMaskWallet({ ...walletsOptions, shimDisconnect: true }),
+  walletConnectWallet(walletsOptions),
+  ledgerWallet(walletsOptions),
+  braveWallet(walletsOptions),
+  coinbaseWallet({ ...walletsOptions, appName: "scaffold-eth-2" }),
+  rainbowWallet(walletsOptions),
+  ...(configuredNetwork.id === chains.hardhat.id || !onlyLocalBurnerWallet
+    ? [burnerWalletConfig({ chains: [appChains.chains[0]] })]
+    : []),
 ];
 
-const safeConnector = new SafeConnector({
-  chains: enabledChains,
-  options: {
-    allowedDomains: [/gnosis-safe.io$/, /app.safe.global$/, /multisig.lol$/],
-    debug: false,
-  },
-});
-
 /**
- * Rainbowkit connectors for the wagmi context
+ * wagmi connectors for the wagmi context
  */
-export const rainbowKitConnectors = connectorsForWallets([
+export const wagmiConnectors = connectorsForWallets([
   {
     groupName: "Supported Wallets",
-    wallets: burnerConfig.enabled ? [...wallets, burnerWalletConfig({ chains: [appChains.chains[0]] })] : wallets,
+    wallets,
   },
 ]);
-
-export const wagmiConnectors = [...rainbowKitConnectors(), safeConnector];
