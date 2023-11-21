a7d9d4a6a513a15d78d299e031ea5f2134b3205e
Zak G <zakgriffith@gmail.com>
2023-11-21 12:09:13 -0700

    Footer - Fix Linting Errors

--- packages/nextjs/components/Footer.tsx
+++ packages/nextjs/components/Footer.tsx
@@ -1,16 +1,17 @@
-import { hardhat } from "wagmi/chains";
-import { CurrencyDollarIcon } from "@heroicons/react/24/outline";
+// import { hardhat } from "wagmi/chains";
+// import { CurrencyDollarIcon } from "@heroicons/react/24/outline";
 import { HeartIcon } from "@heroicons/react/24/outline";
-import { SwitchTheme } from "~~/components/SwitchTheme";
-import { Faucet } from "~~/components/scaffold-eth";
-import { useGlobalState } from "~~/services/store/store";
-import { getTargetNetwork } from "~~/utils/scaffold-eth";
+
+// import { SwitchTheme } from "~~/components/SwitchTheme";
+// import { Faucet } from "~~/components/scaffold-eth";
+// import { useGlobalState } from "~~/services/store/store";
+// import { getTargetNetwork } from "~~/utils/scaffold-eth";
 
 /**
  * Site footer
  */
 export const Footer = () => {
-  const nativeCurrencyPrice = useGlobalState(state => state.nativeCurrencyPrice);
+  // const nativeCurrencyPrice = useGlobalState(state => state.nativeCurrencyPrice);
 
   return (
     <div className="min-h-0 py-5 px-1 mb-11 lg:mb-0">
